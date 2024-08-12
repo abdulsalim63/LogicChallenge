a7cc160155a7d8bea51884388cf1d1ecef9d5095
abdulsalim63 <54874209+abdulsalim63@users.noreply.github.com>
2024-08-12 23:19:39 +0700

    Update Regex Matching solution

--- Regex Matching.cs
+++ Regex Matching.cs
@@ -3,26 +3,96 @@ public class Solution {
         var sCharArray = s.ToCharArray();
         var pCharArray = p.ToCharArray();
 
-        int indexP = 0;
+        bool isSubmit = false;
 
-        for (int i=0; i<s.Length; i++){
-            if (indexP >= p.Length) return false;
+        int indexP = 0;
+        int indexThreshold = 0;
+
+        var result = false;
+
+        for (int i=0; i<20; i++){
+            if (!isSubmit) Console.WriteLine(i);
+            if (!isSubmit) Console.WriteLine(indexP);
+
+            if (i >= s.Length) {
+                var isCharPrevious = false;
+                while (indexP < p.Length) {
+                    if (pCharArray[indexP] == '*') {
+                        isCharPrevious = false;
+                        result = true;
+                    }
+                    else if (pCharArray[indexP] == '.') {
+                        if (isCharPrevious) {
+                            result = false;
+                        }
+                        else result = true;
+
+                        isCharPrevious = false;
+                    }
+                    else {
+                        if (isCharPrevious) break;
+                        isCharPrevious = true;
+                        result = false;
+                    }
+
+                    indexP++;
+                }
+                break;
+            }
+
+            if (indexP >= p.Length) {
+                if (i >= s.Length) {
+                    if (!isSubmit) Console.WriteLine("reach p and s index");
+                    break;
+                }
+                else {
+                    if (!isSubmit) Console.WriteLine(sCharArray[i]);
+                    if (!isSubmit) Console.WriteLine("reach p index with s left");
+                    result = false;
+                    break;
+                }
+            }
+            
+            if (!isSubmit) Console.WriteLine(sCharArray[i]);
+            if (!isSubmit) Console.WriteLine(pCharArray[indexP]);
 
             if (pCharArray[indexP] == '.' || sCharArray[i] == pCharArray[indexP]){
+                if (!isSubmit) Console.WriteLine("same character or .");
+                result = true;
                 indexP++;
             }
             else if (pCharArray[indexP] == '*'){
-                if (pCharArray[indexP - 1] == '.') return true;
+                if (pCharArray[indexP - 1] == '.'){
+                    if (indexP + 1 >= p.Length) {
+                        if (!isSubmit) Console.WriteLine("last p character is .*");
+                        return true;
+                    }
+                    else{
+                        // check if the rest is match between s and p
+                        indexThreshold = i;
+                        // temp
+                        return true;
+                    }
+                }
+                else if (sCharArray[i] == pCharArray[indexP - 1]) {
+                    if (!isSubmit) Console.WriteLine("* with match previous char");
+                    result = true;
+                }
                 else if (sCharArray[i] != pCharArray[indexP - 1]) {
+                    if (!isSubmit) Console.WriteLine("* with doesn't match previous char");
+                    result = false;
                     indexP++;
                     i--;
                 }
             }
             else {
                 indexP++;
-                if (indexP >= p.Length) return false;
+                if (indexP >= p.Length) {
+                    if (!isSubmit) Console.WriteLine("char doesn't match and last index");
+                }
 
                 if (pCharArray[indexP] == '*') {
+                    if (!isSubmit) Console.WriteLine("char doesn't match and next s char is *");
                     indexP++;
                     i--;
                     continue;
@@ -30,6 +100,6 @@ public class Solution {
             }
         }
 
-        return true;
+        return result;
     }
 }
