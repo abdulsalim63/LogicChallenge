5da031fc18f23f53bf76221b9cff2aa68d6da8f0
abdulsalim63 <54874209+abdulsalim63@users.noreply.github.com>
2024-08-13 22:37:03 +0700

    Update Regex Matching Solution

--- Regex Matching.cs
+++ Regex Matching.cs
@@ -6,7 +6,7 @@ public class Solution {
         bool isSubmit = false;
 
         int indexP = 0;
-        int indexThreshold = 0;
+        var indexThreshold = new Dictionary<int, int>();
 
         var result = false;
 
@@ -15,8 +15,10 @@ public class Solution {
             if (!isSubmit) Console.WriteLine(indexP);
 
             if (i >= s.Length) {
-                var isCharPrevious = false;
+                if (!isSubmit) Console.WriteLine("reach max index s");
+                var isCharPrevious = pCharArray[indexP - 1] != '*';
                 while (indexP < p.Length) {
+                    if (!isSubmit) Console.WriteLine(isCharPrevious + " " + pCharArray[indexP]);
                     if (pCharArray[indexP] == '*') {
                         isCharPrevious = false;
                         result = true;
@@ -30,6 +32,46 @@ public class Solution {
                         isCharPrevious = false;
                     }
                     else {
+                        indexP++;
+
+                        if (indexP < p.Length && pCharArray[indexP] == '*'){
+                            indexP++;
+                            continue;
+                        }
+
+                        if (indexThreshold.Any()) {
+                            if (!isSubmit) Console.WriteLine("got indexThreshold value " + string.Join(", ", indexThreshold.Select(s => $"{s.Key}={s.Value}")));
+                            indexP--;
+                            result = false;
+
+                            while (!result && indexThreshold.Any()) {
+                                if (!indexThreshold.Any()) return false;
+
+                                var lastIndexDict = indexThreshold.Last();
+
+                                for (int j=i-1; j>lastIndexDict.Key; j--) {
+                                    if (sCharArray[j] == pCharArray[indexP]) {
+                                        if (!isSubmit) Console.WriteLine("element match checking index threshold " + pCharArray[indexP] + " " + sCharArray[j]);
+                                        i = j;
+                                        indexThreshold.Add(j, indexP);
+                                        indexP++;
+                                        result = true;
+                                        break;
+                                    }
+                                }
+
+                                if (result) break;
+
+                                indexThreshold.Remove(lastIndexDict.Key);
+                                indexP = lastIndexDict.Value;
+                            }
+
+                            continue;
+                        }
+                        else {
+                            return false;
+                        }
+
                         if (isCharPrevious) break;
                         isCharPrevious = true;
                         result = false;
@@ -41,7 +83,11 @@ public class Solution {
             }
 
             if (indexP >= p.Length) {
-                if (i >= s.Length) {
+                if (pCharArray[indexP - 1] == '*' && result) {
+                    if (!isSubmit) Console.WriteLine("reach p but previous *");
+                    indexP--;
+                }
+                else if (i >= s.Length) {
                     if (!isSubmit) Console.WriteLine("reach p and s index");
                     break;
                 }
@@ -56,6 +102,91 @@ public class Solution {
             if (!isSubmit) Console.WriteLine(sCharArray[i]);
             if (!isSubmit) Console.WriteLine(pCharArray[indexP]);
 
+            if (sCharArray[i] == pCharArray[indexP] || pCharArray[indexP] == '.') {
+                if (!isSubmit) Console.WriteLine("match: " + sCharArray[i] + " " + pCharArray[indexP]);
+                result = true;
+                indexP++;
+
+                if (indexP >= p.Length) continue;
+
+                if (pCharArray[indexP] != '*') {// && pCharArray[indexP - 1] != '.'){
+                    indexThreshold.Add(i, indexP - 1);
+                }
+
+                continue;
+            }
+            else if (pCharArray[indexP] == '*') {
+                if (!isSubmit) Console.WriteLine("* on element p");
+                if (pCharArray[indexP - 1] == '.') {
+                    result = true;
+                    if (!isSubmit) Console.WriteLine("previous element match: " + pCharArray[indexP - 1]);
+                    indexP--;
+                }
+                else if (sCharArray[i] == pCharArray[indexP - 1]) {
+                    result = true;
+                    if (!isSubmit) Console.WriteLine("previous element match: " + pCharArray[indexP - 1]);
+                }
+                else {
+                    result = false;
+                    i--;
+                    if (!isSubmit) Console.WriteLine("previous element doesn't match");
+                }
+
+                indexP++;
+            }
+            else {
+                indexP++;
+                if (!isSubmit) Console.WriteLine("element doesn't match");
+
+                if (indexP < p.Length && pCharArray[indexP] == '*'){
+                    indexP++;
+                    i--;
+                    continue;
+                }
+
+                if (indexThreshold.Any()) {
+                    if (!isSubmit) Console.WriteLine("got indexThreshold value " + string.Join(", ", indexThreshold.Select(s => $"{s.Key}={s.Value}")));
+                    indexP--;
+                    // Check up until before the last indexThreshold
+                    // if it's 3, check only until 4
+                    // if still doesn't find a match, remove the last index
+                    // then do it again with the new last index
+                    // until empty, then return false
+                    // if found match, add this new index into index Threshold
+
+                    // do it again with the next element on p
+                    result = false;
+
+                    while (!result) {
+                        if (!indexThreshold.Any()) return false;
+
+                        var lastIndexDict = indexThreshold.Last();
+
+                        for (int j=i-1; j>lastIndexDict.Key; j--) {
+                            if (sCharArray[j] == pCharArray[indexP]) {
+                                if (!isSubmit) Console.WriteLine("element match checking index threshold " + pCharArray[indexP] + " " + sCharArray[j]);
+                                i = j;
+                                indexThreshold.Add(j, indexP);
+                                indexP++;
+                                result = true;
+                                break;
+                            }
+                        }
+
+                        if (result) break;
+
+                        indexThreshold.Remove(lastIndexDict.Key);
+                        indexP = lastIndexDict.Value;
+                    }
+
+                    continue;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            /*
             if (pCharArray[indexP] == '.' || sCharArray[i] == pCharArray[indexP]){
                 if (!isSubmit) Console.WriteLine("same character or .");
                 result = true;
@@ -97,9 +228,27 @@ public class Solution {
                     i--;
                     continue;
                 }
-            }
+            }*/
         }
 
         return result;
     }
 }
+
+// Testcase
+/*"aa"
+"a"
+"aa"
+"a*"
+"ab"
+".*"
+"caab"
+"c*a*b."
+"caab"
+"c*a*b*."
+"aaa"
+".*"
+"abcd"
+"d*"
+"a"
+"a*a"*/
